ee2359c2d7923c27f7708295928f8e542bea31d4
nitinthakur404 <nitinthakur404@gmail.com>
2021-06-17 17:20:25 +0530

    second commit and create folder for project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';import reportWebVitals from './reportWebVitals';
+import Menu from './project/project1/Menu'
+
 
 ReactDOM.render(
   <>
-
+    <Menu />
   </>,
   document.getElementById('root')
 );
@@ -13,4 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/project/project1/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function menu() {
+    return (
+        <div>
+            <h1>my name is nitin kumar</h1>
+        </div>
+    )
+}
